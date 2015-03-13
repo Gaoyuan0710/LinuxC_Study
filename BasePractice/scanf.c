438a55cbecb235b00f842ebff1ee3b1620c7d927
Gaoyuan0710 <sishuiliunian0710@gmail.com>
2015-03-13 16:43:56 +0800

    add file scanf.c

---
+++ BasePractice/scanf.c
@@ -0,0 +1,30 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  scanf.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Gaoyuan, sishuiliunian0710@gmail.com
+ *        Company:  Class 1204 of Computer Science and Technology
+ *
+ * =====================================================================================
+ */
+
+#include <unistd.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+
+
+	return EXIT_SUCCESS;
+}
+
